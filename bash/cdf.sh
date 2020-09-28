cfb9912ae1db6f9fbb778e11470972f0e9b5dbf2
dushyant <dushmis@users.noreply.github.com>
2020-09-29 00:06:56 +0530

    Update cdf.sh

--- bash/cdf.sh
+++ bash/cdf.sh
@@ -1,7 +1,7 @@
 # cd to a file
 # cdf /tmp/file/where/you/want/to/go.txt
 # it'll go to /tmp/file/where/you/want/to/ folder
-# put this in .zshrc or .bashrc, source this or added it in those files
+# put this in .zshrc or .bashrc, source this or add it in those files
 cdf() {
   echo "$(dirname "$1")";
   cd "$(dirname "$1")";
