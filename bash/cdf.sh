65c1c49dea9db93b96e9b4bf02a24c68ba4f7bb7
Dushyant Mistry <dushyant.mistry@youbroadband.co.in>
2020-09-29 00:05:45 +0530

    cdf

---
+++ bash/cdf.sh
@@ -0,0 +1,8 @@
+# cd to a file
+# cdf /tmp/file/where/you/want/to/go.txt
+# it'll go to /tmp/file/where/you/want/to/ folder
+# put this in .zshrc or .bashrc, source this or added it in those files
+cdf() {
+  echo "$(dirname "$1")";
+  cd "$(dirname "$1")";
+}
